2b9fec4d7ebbe7cbc3b84c4221cafd4319b3b501
EricaMarzi <erica.marzi.1993@gmail.com>
2024-03-07 19:25:22 +0100

    css header import

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -11,34 +11,34 @@
 </head>
 
 <body>
-    <div class="container">
-        <div class="logo">
-            <img src="{{asset('images/dc-logo.png')}}" alt="logo">
-        </div>
+    <header>
+        <div class="container">
+            <div class="logo">
+                <img src="{{asset('images/dc-logo.png')}}" alt="logo">
+            </div>
 
-        <div>
-            <header>
-                <ul class="navbar">
-                    <li><a href="{{route('char')}}">Characters</a></li>
-                    <li><a href="{{route('comics')}}">Comics</a></li>
-                    <li><a href="{{route('movie')}}">Movie</a></li>
-                    <li><a href="{{route('TV')}}">Tv</a></li>
-                    <li><a href="{{route('games')}}">Games</a></li>
-                    <li><a href="{{route('collectibles')}}">Collectibles</a></li>
-                    <li><a href="{{route('videos')}}">Videos</a></li>
-                    <li><a href="{{route('fans')}}">Fans</a></li>
-                    <li><a href="{{route('news')}}">News</a></li>
-                    <li><a href="{{route('shop')}}">Shop</a></li>
-                </ul>
-            </header>
-            <main>
-                comics
-            </main>
-            <footer>
-                footer
-            </footer>
+            <ul class="navbar">
+                <li><a href="{{route('char')}}">Characters</a></li>
+                <li><a href="{{route('comics')}}">Comics</a></li>
+                <li><a href="{{route('movie')}}">Movie</a></li>
+                <li><a href="{{route('TV')}}">Tv</a></li>
+                <li><a href="{{route('games')}}">Games</a></li>
+                <li><a href="{{route('collectibles')}}">Collectibles</a></li>
+                <li><a href="{{route('videos')}}">Videos</a></li>
+                <li><a href="{{route('fans')}}">Fans</a></li>
+                <li><a href="{{route('news')}}">News</a></li>
+                <li><a href="{{route('shop')}}">Shop</a></li>
+            </ul>
         </div>
-    </div>
+    </header>
+    <main>
+
+    </main>
+    <footer>
+
+    </footer>
+
+
 </body>
 
 </html>
