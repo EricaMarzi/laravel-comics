96d967c6ea7d583389f1e2dfcb9788a37ddfad24
EricaMarzi <erica.marzi.1993@gmail.com>
2024-03-07 18:00:08 +0100

    first commit

---
+++ resources/views/tv.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DC | Characters</title>
+    <link rel="icon" href="./src/assets/dc-comics-1/img/dc-logo.png">
+</head>
+
+<body>
+    TV
+</body>
+
+</html>
---
+++ routes/web.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/characters', function () {
+    return view('characters');
+})->name('char');
+
+Route::get('/', function () {
+    return view('comics');
+})->name('comics');
+
+Route::get('/movie', function () {
+    return view('movie');
+})->name('movie');
+
+Route::get('/tv', function () {
+    return view('TV');
+})->name('TV');
+
+Route::get('/games', function () {
+    return view('games');
+})->name('games');
+
+Route::get('/collectibles', function () {
+    return view('collectibles');
+})->name('collectibles');
+
+Route::get('/videos', function () {
+    return view('videos');
+})->name('videos');
+
+Route::get('/fans', function () {
+    return view('fans');
+})->name('fans');
+
+Route::get('/news', function () {
+    return view('news');
+})->name('news');
+
+Route::get('/shop', function () {
+    return view('shop');
+})->name('shop');
