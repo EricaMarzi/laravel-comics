7649c716fd456d8ec8cf66902107ad9a9ba33328
EricaMarzi <erica.marzi.1993@gmail.com>
2024-03-07 22:25:43 +0100

    main

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -1,4 +1,17 @@
 @extends('layouts.main')
 @section('main-content')
-main
+<div class="container">
+    <div class="row-main">
+        <div class="col-main">
+            @foreach($comics as $comic)
+            <div class="card-main">
+                <img class="poster" src="{{$comic['thumb']}}" alt="{{$comic['title']}}">
+                <label>
+                    <div class="title">{{$comic['series']}}</div>
+                </label>
+            </div>
+            @endforeach
+        </div>
+    </div>
+</div>
 @endsection
